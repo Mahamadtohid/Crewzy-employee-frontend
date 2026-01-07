98e9ed44a3540e6ffd526f46a7f8f4aa1674c35c
Mahamadtohid <naikwadimdtohid@gmail.com>
2026-01-07 23:23:43 +0530

    Implemented Dashboard and Login Pages 07/01/26

---
+++ src/pages/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import API from "../services/api";
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("username", username);
+    formData.append("password", password);
+
+    try {
+      const res = await API.post("/login", formData);
+      localStorage.setItem("token", res.data.access_token);
+      onLogin();
+    } catch (err) {
+      alert("Invalid credentials");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
